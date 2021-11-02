c84b922b64c51b24742e3d683abd1c1409347f49
joseangelgalera <joseangelgg96@hotmail.com>
2021-11-02 12:52:50 +0100

    Añadiendo cambios al diseño del LoginActivity.kt

--- app/src/main/java/com/example/flixnet/LoginActivity.kt
+++ app/src/main/java/com/example/flixnet/LoginActivity.kt
@@ -2,10 +2,15 @@ package com.example.flixnet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    fun login(){
+        Log.i("xxx", "HAS PULSADO EL BOTON DE LOGGIN")
     }
 }
